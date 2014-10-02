64becf566919253de1fd047b750e87772733d6a7
Anh Do <dotuanhoanganh@gmail.com>
2014-10-02 23:20:07 +1000

    show Toast when after choice is added

--- src/com/anhdo/android/vote/ChoiceActivity.java
+++ src/com/anhdo/android/vote/ChoiceActivity.java
@@ -1,19 +1,47 @@
 package com.anhdo.android.vote;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ChoiceActivity extends ActionBarActivity {
+	private EditText mChoiceEditText;
+	private Button mAddButton;  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        
+        mChoiceEditText = (EditText)findViewById(R.id.choice);
+        mAddButton = (Button)findViewById(R.id.add_button);
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				saveChoice();
+				showToast(R.string.choice_added_toast);
+				clearEditText();
+			}
+		});
     }
-
+    
+    private void saveChoice() {
+    	
+    }
+    
+    private void showToast(int text) {
+    	Toast.makeText(ChoiceActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+    
+    private void clearEditText() {
+    	mChoiceEditText.setText("");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
