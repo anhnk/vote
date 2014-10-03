b642c5033c316b04693b322ab774f806a46e6cab
Anh Do <dotuanhoanganh@gmail.com>
2014-10-04 05:02:40 +1000

    Remove menu placeholder methods

--- src/com/anhdo/android/vote/ChoiceActivity.java
+++ src/com/anhdo/android/vote/ChoiceActivity.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -66,24 +64,5 @@ public class ChoiceActivity extends ActionBarActivity {
     
     private void clearEditText() {
     	mChoiceEditText.setText("");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.choice, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
