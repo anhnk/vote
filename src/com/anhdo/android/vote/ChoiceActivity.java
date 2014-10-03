635497b06321cffc795d872fee6e472a31367618
Anh Do <dotuanhoanganh@gmail.com>
2014-10-04 04:58:51 +1000

    Wire up Add, Vote, Clear buttons in ChoiceActivity

--- src/com/anhdo/android/vote/ChoiceActivity.java
+++ src/com/anhdo/android/vote/ChoiceActivity.java
@@ -1,5 +1,8 @@
 package com.anhdo.android.vote;
 
+import java.util.ArrayList;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,7 +15,10 @@ import android.widget.Toast;
 
 public class ChoiceActivity extends ActionBarActivity {
 	private EditText mChoiceEditText;
-	private Button mAddButton;  
+	private Button mAddButton;
+	private Button mVoteButton;
+	private Button mClearButton;
+	private ArrayList<Choice> mChoices = new ArrayList<Choice>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +35,29 @@ public class ChoiceActivity extends ActionBarActivity {
 				clearEditText();
 			}
 		});
+        
+        mVoteButton = (Button)findViewById(R.id.vote_button);
+        mVoteButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(ChoiceActivity.this, VoteActivity.class);
+				i.putParcelableArrayListExtra("choice", mChoices);
+				startActivity(i);
+			}
+		});
+        
+        mClearButton = (Button)findViewById(R.id.clear_button);
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mChoices.clear();
+			}
+		});
     }
     
     private void saveChoice() {
-    	
+    	Choice choice = new Choice(mChoiceEditText.getText().toString(), 0);
+    	mChoices.add(choice);
     }
     
     private void showToast(int text) {
